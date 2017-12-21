70b233c92512bcc4105e315290f8220cfd4d49e5
munukutlamouli <34467955+munukutlamouli@users.noreply.github.com>
2017-12-21 12:43:22 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,19 @@
+package mouli;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int [] arr ={1,2,3,4,5};
+		
+		int sum =0;
+		
+		for(int i =0; i<=4; i++)
+		{
+			sum+= arr[i];
+			
+			System.out.println("sum of arrays is " + sum);
+		}
+
+	}
+
+}
